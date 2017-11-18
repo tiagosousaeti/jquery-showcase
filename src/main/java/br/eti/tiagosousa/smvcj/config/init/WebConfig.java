d20a464a5c8f80b4fb5518900c26557d56756502
Tiago Sousa <contato@tiagosousa.eti.br>
2017-11-18 01:03:50 -0200

    Habilitando o MVC

--- src/main/java/br/eti/tiagosousa/smvcj/config/init/WebConfig.java
+++ src/main/java/br/eti/tiagosousa/smvcj/config/init/WebConfig.java
@@ -1,9 +1,13 @@
 package br.eti.tiagosousa.smvcj.config.init;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 import br.eti.tiagosousa.smvcj.controller.HomeController;
 
@@ -11,4 +15,13 @@ import br.eti.tiagosousa.smvcj.controller.HomeController;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public ViewResolver internalResourceViewResolver() {
+	    InternalResourceViewResolver bean = new InternalResourceViewResolver();
+	    bean.setViewClass(JstlView.class);
+	    bean.setPrefix("/templates/views/");
+	    bean.setSuffix(".jsp");
+	    return bean;
+	}
 }
